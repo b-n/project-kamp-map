7a22f0dc777bb681c93a469bfcb6e4ca262ef92c
Ben Naylor <nayben@gmail.com>
2021-04-18 15:59:56 +0200

    Remove unused polyfill (will use babel if really needing promises)

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import LeafletMap from './js/map'
 import './sass/index.scss'
 
 document.addEventListener('DOMContentLoaded', () => {
-  if (!window.Promise) {
-    window.Promise = Promise
-  }
-
   const mapElement = document.getElementById('map')
 
   const map = new LeafletMap()
